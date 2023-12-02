8d772f34fae732e232ff29dae8e725f91aea87e7
Paulina <paulalastowiecka@gmail.com>
2023-12-02 18:37:16 +0100

    app done

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "tailwindcss"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
+	base: "/advent-calendar/",
+	plugins: [react()],
+	css: {
+		postcss: {
+			plugins: [tailwindcss()],
+		},
+	},
 })
-
