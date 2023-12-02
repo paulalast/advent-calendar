799914267cdefe3ec4ccbab421879d69bc2bf4d1
Paulina <paulalastowiecka@gmail.com>
2023-12-02 20:18:36 +0100

    new

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,27 @@ import react from "@vitejs/plugin-react"
 import tailwindcss from "tailwindcss"
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/advent-calendar/",
 	plugins: [react()],
+	base: "./",
 	css: {
 		postcss: {
 			plugins: [tailwindcss()],
 		},
 	},
 })
+
+// import { defineConfig } from "vite"
+// import react from "@vitejs/plugin-react"
+// import { terser } from "rollup-plugin-terser"
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+// 	plugins: [react()],
+// 	base:"/v-wellbeing-app/",
+// 	build: {
+// 		minify: "terser",
+// 		rollupOptions: {
+// 			plugins: [terser()],
+// 		},
+// 	},
+// })
